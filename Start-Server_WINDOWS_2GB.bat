31608667d5f1d303afe2dd91efab8b9f66692546
Christopher <christopher.bersuch@gmail.com>
2018-02-28 23:10:28 +0100

    654

---
+++ Start-Server_WINDOWS_2GB.bat
@@ -0,0 +1,2 @@
+cd Server
+start javaw -Xms2048m -Xmx2048m -jar forge-1.12.2-14.23.2.2611-universal.jar
---
+++ Start-Server_WINDOWS_3GB.bat
@@ -0,0 +1,2 @@
+cd Server
+start javaw -Xms3072m -Xmx3072m -jar forge-1.12.2-14.23.2.2611-universal.jar
